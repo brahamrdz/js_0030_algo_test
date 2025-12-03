0f996db8c07eb6dd0622ec2b1af1cd1c20afe64f
brahamrdz <100656093+brahamrdz@users.noreply.github.com>
2025-12-03 10:22:24 -0600

    Implement sumaEntreNumeros function logic
    Fix sumaEntreNumeros function to correctly sum numbers between a and b.

--- app.js
+++ app.js
@@ -31,7 +31,12 @@ export function mayorDeTresNumeros(a, b, c) {
 }
 // 4 funcion para devolver la suma de los numero entre a y b
 export function sumaEntreNumeros(a, b) {
-  return 0
+  let suma = 0;
+  for (let i = a+1; i < b; i++) {
+      suma += i;
+  }
+  
+  return suma;
 }
 // 5 funcion para devolver el numero de numeros pares entre a y b
 export function numerosParesCount(a, b) {
